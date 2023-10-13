37d5404434869dfb379ef63625e4db1b0428dced
M2dave <147533608+M2dave@users.noreply.github.com>
2023-10-13 18:23:14 +0330

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cout << "Enter a four-digit number: ";
+	cin >> number;
+
+	int sum = 0;
+	while (number > 0) {
+		sum += number % 10; 
+		number /= 10;
+	}
+
+	cout << "Sum of digits: " << sum << endl;
+
+	return 0;
+}
